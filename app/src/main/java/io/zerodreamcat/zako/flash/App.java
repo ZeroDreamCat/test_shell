efbceebc56254abb345873acb8f2803fe5fc4f54
SoraNeko <74408119+ZeroDreamCat@users.noreply.github.com>
2026-04-11 00:48:13 +0800

    Update App.java

--- app/src/main/java/io/zerodreamcat/zako/flash/App.java
+++ app/src/main/java/io/zerodreamcat/zako/flash/App.java
@@ -2,11 +2,13 @@ package io.zerodreamcat.zako.flash;
 
 import android.app.Application;
 import android.content.Context;
+import com.topjohnwu.superuser.Shell;
 
 public class App extends Application {
     public static final String TAG = "ZakoFlash";
     public static Context application;
     public static IRemoteService server;
+    public static Shell rootShell;
 
     public App() {
         application = this;
